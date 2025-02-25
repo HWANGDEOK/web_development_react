35740e75605c197b50874f97cc76c1c627c93bea
HWANGDEOK <dukjun2002@naver.com>
2025-02-25 21:45:39 +0900

    feat: 20250225 React lesson started

---
+++ vite-project/src/App2.jsx
@@ -0,0 +1,16 @@
+function App2(){
+  return (
+    // <div>
+    //   <h1>Hello World</h1>
+    //   <h2>첫 번째 리액트 컴퍼넌트입니다.</h2>
+    // </div>
+    <>
+        <div>
+      <h1>Hello World</h1>
+      <h2>첫 번째 리액트 컴퍼넌트입니다.</h2>
+    </div>
+    </>
+  );
+}
+
+export default App2
