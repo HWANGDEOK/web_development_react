bf987ff1c1284b495f6cb76a4d4f9ede3628ecc9
HWANGDEOK <dukjun2002@naver.com>
2025-02-26 21:44:25 +0900

    feat: 20250226 useState lesson

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
-import App2 from './App2.jsx'
+// import App2 from './App2.jsx'
+// import Hello2 from './Hello.jsx'
+// import Hello3 from './Hello.jsx'
+// import Hello4 from './Hello.jsx'
+// import App3 from './App3.jsx'
+// import HeaderText from './App4.jsx'
+import Counter from './App5'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <App2 />
+    {/* <App2 /> */}
+    {/* <Hello2 user="Ahn"/>  */}
+    {/* <Hello3 firstName="Robert" lastName="Peterson"/> */}
+    {/* <Hello4 firstName="로버트" lastName="패틴슨"/> */}
+    {/* <App3 /> */}
+    {/* <HeaderText text="저는 오늘 방특이 끝났습니다!"/> */}
+    <Counter />
   </React.StrictMode>,
 )
