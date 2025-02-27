00e11ebb3f57579f05dde251c4d873de9dd915d1
HWANGDEOK <dukjun2002@naver.com>
2025-02-27 21:31:31 +0900

    feat: 20250227 useRef / custom Hook / context API

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -7,7 +7,11 @@ import ReactDOM from 'react-dom/client'
 // import Hello4 from './Hello.jsx'
 // import App3 from './App3.jsx'
 // import HeaderText from './App4.jsx'
-import Counter from './App5'
+// import Counter from './App5'
+// import App6 from './App6'
+// import Counter from './App7'
+// import App8 from './App8'
+import Counter from './App9'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -19,6 +23,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <Hello4 firstName="로버트" lastName="패틴슨"/> */}
     {/* <App3 /> */}
     {/* <HeaderText text="저는 오늘 방특이 끝났습니다!"/> */}
+    {/* <Counter /> */}
+    {/* <App6 /> */}
+    {/* <Counter /> */}
+    {/* <App8 /> */}
     <Counter />
   </React.StrictMode>,
 )
