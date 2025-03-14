4f68f8c7fbc2e962d7c75961cabedae2c804fde6
HWANGDEOK <dukjun2002@naver.com>
2025-03-14 21:35:25 +0900

    feat: 20250314 useState / 삼항연산자 / 객체조회방식 두 가지

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from "react";
 import CoreConcepts from "./components/CoreConcepts";
 import Header2 from "./components/Header/Header2";
 import TabButton from "./components/TabButton";
-import { CORE_CONCEPTS } from "./data"
+import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES } from "./data_updated";
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState('');
 
-  // const [ selectedTopic, setSelectedTopic ] = useState('');
-
-  function handSelect(selectedTopic){
+  function handleSelect(selectedTopic) {
     // selectedButton = 'components', 'jsx', 'props', 'state'
-    console.log(selectedTopic);
+    // console.log(selectedTopic);
+    setSelectedTopic(selectedTopic);
   }
 
 
@@ -30,16 +31,25 @@ function App() {
     <section id="examples">
       <h2>예시들</h2>
       <menu>
-        <TabButton onSelect={handSelect('굿나잇')}>Components</TabButton>
-        <TabButton>JSX</TabButton>
-        <TabButton>Props</TabButton>
-        <TabButton>State</TabButton>
+        <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+        <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
         {/* <TabButton label='Components' />
         <TabButton label='JSX' />
         <TabButton label='Props' />
         <TabButton label='State' /> */}
       </menu>
-      동적인 버튼 클릭의 결과물
+      {selectedTopic === '' ? <h3>버튼을 클릭하세요</h3> :
+            <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic]['title']}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div> }
     </section>
       </main>
     </div>
