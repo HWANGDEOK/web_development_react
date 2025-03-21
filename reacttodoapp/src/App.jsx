4a4a12ad4fdb3fc7c05a9b54a0cab742c0b0b0ba
HWANGDEOK <dukjun2002@naver.com>
2025-03-21 21:33:39 +0900

    feat: 20250321 spread syntax / .map() method / todolist

--- reacttodoapp/src/App.jsx
+++ reacttodoapp/src/App.jsx
@@ -1,12 +1,41 @@
+import { useState } from 'react';
 import './App.css'
-
 function App() {
+  const [ toDo, setToDo] = useState('');
+  const [ toDos, setToDos] = useState([]);
+
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+
+  const onSubmit = (event)=>{
+    event.preventDefault();
+
+    if(toDo === ''){
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    console.log(toDos);
+    setToDo('');
+  }
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>My To Dos</h1>
+      <form onSubmit={onSubmit}>
+        <input
+        type="text"
+        onChange={onChange}
+        placeholder="Type Your To Do 🔍"
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      <ul>
+        {toDos.map((item, idx) => <li key={idx}>{item}</li>)}
+      </ul>
+    </div>
   )
 }
 
-export default App
+export default App;
